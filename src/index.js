35835ac7e48e68dad6f0b5ddb2de9d29c90d5b13
Magomed <magomed-gasanov1996@yandex.ru>
2020-03-22 22:13:48 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+
+function towelSort (matrix) {
+  let arr = [];
+
+  if (!Array.isArray(matrix) || matrix == []) {
+    return arr;
+  }
+
+  matrix.forEach(function (item, index) {
+    if ((index+1)%2 != 0) {
+      Array.prototype.push.apply(arr,item)
+    } else {
+      Array.prototype.push.apply(arr,item.reverse())
+    }
+  }
+  )
+
+  return arr
 }
+
+module.exports = towelSort;
+
